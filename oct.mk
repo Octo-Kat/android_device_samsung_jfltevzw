4010145d9f73a5b6e868413e64735225f69f20a4
treken <chupekevin@gmail.com>
2014-01-28 08:45:41 -0600

    Add apns config.

--- oct.mk
+++ oct.mk
@@ -1,5 +1,9 @@
 $(call inherit-product, device/samsung/jfltevzw/full_jfltevzw.mk)
 
+# Inherit some common OctOS stuff.
+$(call inherit-product, vendor/oct/config/cdma.mk)
+$(call inherit-product, vendor/oct/config/common_full_phone.mk)
+
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jfltevzw TARGET_DEVICE=jfltevzw BUILD_FINGERPRINT="Verizon/jfltevzw/jfltevzw:4.2.2/JDQ39/I545VRUAMDK:user/release-keys" PRIVATE_BUILD_DESC="jfltevzw-user 4.2.2 JDQ39 I545VRUAMDK release-keys"
 
 PRODUCT_NAME := oct_jfltevzw
